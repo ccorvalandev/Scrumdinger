c467e01b5d6f0db105a920ec75e79800b18b932e
Carlos Corvalan <ccorvalandev@outlook.com>
2022-05-03 13:49:49 -0400

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Carlos Corvalan on 5/3/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
