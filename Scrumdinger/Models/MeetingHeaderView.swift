41b21dedb5d507da4175e256a9977dd8b1e0e511
Carlos Corvalan <ccorvalandev@outlook.com>
2022-05-09 14:25:56 -0400

    Extract the Meeting Header

---
+++ Scrumdinger/Models/MeetingHeaderView.swift
@@ -0,0 +1,50 @@
+//
+//  MeetingHeaderView.swift
+//  Scrumdinger
+//
+//  Created by Carlos Corvalan on 5/9/22.
+//
+
+import SwiftUI
+
+struct MeetingHeaderView: View {
+    let secondsElapsed: Int
+    let secondsRemaining: Int
+    private var totalSeconds: Int {
+        secondsElapsed + secondsRemaining
+    }
+    private var progress: Double {
+        guard totalSeconds > 0 else { return 1 }
+        return Double(secondsElapsed) / Double(totalSeconds)
+    }
+    private var minutesRemaining: Int {
+        secondsRemaining / 60
+    }
+    var body: some View {
+        VStack{
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(.caption)
+                    Label("\(secondsElapsed)", systemImage: "hourglass.bottomhalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("\(secondsRemaining)", systemImage: "hourglass.tophalf.fill")
+                }
+            }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time remaining")
+            .accessibilityValue("\(minutesRemaining) minutes")
+        }
+    }
+}
+
+struct MeetingHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingHeaderView(secondsElapsed: 300, secondsRemaining: 600)
+    }
+}
