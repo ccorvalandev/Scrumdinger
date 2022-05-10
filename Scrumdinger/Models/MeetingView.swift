80710278a4836dce81d011c39598a542a90c0da7
Carlos Corvalan <ccorvalandev@outlook.com>
2022-05-10 11:19:27 -0400

    Extract the Meeting Footer

--- Scrumdinger/Models/MeetingView.swift
+++ Scrumdinger/Models/MeetingView.swift
@@ -17,15 +17,8 @@ struct MeetingView: View {
             VStack {
                 MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed, secondsRemaining: scrumTimer.secondsRemaining, theme: scrum.theme)
                 Circle()
-                    .strokeBorder(lineWidth: 24)
-                HStack {
-                    Text("Speaker 1 of 3")
-                    Spacer()
-                    Button(action: {}) {
-                        Image(systemName: "forward.fill")
-                    }
-                    .accessibilityLabel("Next speaker")
-                }
+                    .strokeBorder(lineWidth: 24, antialiased: true)
+                MeetingFooterView(speakers: scrumTimer.speakers, skipAction: scrumTimer.skipSpeaker)
             }
             .padding()
             .foregroundColor(scrum.theme.accentColor)
